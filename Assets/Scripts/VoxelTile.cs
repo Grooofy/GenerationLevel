6610123516bf30fa163b538a9db819ec82dd4dee
userUnity <unitvladimir@gmail.com>
2022-09-14 20:55:18 +1000

    Lesson2

--- Assets/Scripts/VoxelTile.cs
+++ Assets/Scripts/VoxelTile.cs
@@ -1,47 +1,45 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
+
 
 public class VoxelTile : MonoBehaviour
 {
-    [SerializeField] private float _voxelSize = 0.1f;
-    [SerializeField] private int _tileSideVoxels = 8;
+    [SerializeField] public float VoxelSize = 0.1f;
+    [SerializeField] public int TileSideVoxels = 8;
 
     [HideInInspector] public byte[] ColorRight;
     [HideInInspector] public byte[] ColorForward;
     [HideInInspector] public byte[] ColorLeft;
     [HideInInspector] public byte[] ColorBack;
 
-    private void Start()
+    [Range(1,100)]   
+    public int Weight = 50;
+
+    public void CalculateSideColors()
     {
-        ColorRight = new byte[_tileSideVoxels * _tileSideVoxels];
-        ColorForward = new byte[_tileSideVoxels * _tileSideVoxels];
-        ColorLeft = new byte[_tileSideVoxels * _tileSideVoxels];
-        ColorBack = new byte[_tileSideVoxels * _tileSideVoxels];
+        ColorRight = new byte[TileSideVoxels * TileSideVoxels];
+        ColorForward = new byte[TileSideVoxels * TileSideVoxels];
+        ColorLeft = new byte[TileSideVoxels * TileSideVoxels];
+        ColorBack = new byte[TileSideVoxels * TileSideVoxels];
         
-        for (int y = 0; y < _tileSideVoxels; y++)
+        for (int y = 0; y < TileSideVoxels; y++)
         {
-            for (int i = 0; i < _tileSideVoxels; i++)
+            for (int i = 0; i < TileSideVoxels; i++)
             {
-                ColorRight[y * _tileSideVoxels + i] = GetVoxelColor(y, i, Vector3.right);
-                ColorForward[y * _tileSideVoxels + i] = GetVoxelColor(y, i, Vector3.forward);
-                ColorLeft[y * _tileSideVoxels + i] = GetVoxelColor(y, i, Vector3.left);
-                ColorBack[y * _tileSideVoxels + i] = GetVoxelColor(y, i, Vector3.back);
+                ColorRight[y * TileSideVoxels + i] = GetVoxelColor(y, i, Vector3.right);
+                ColorForward[y * TileSideVoxels + i] = GetVoxelColor(y, i, Vector3.forward);
+                ColorLeft[y * TileSideVoxels + i] = GetVoxelColor(y, i, Vector3.left);
+                ColorBack[y * TileSideVoxels + i] = GetVoxelColor(y, i, Vector3.back);
             }
         }
-        
-        Debug.Log(string.Join(", ", ColorRight));
     }
 
     private byte GetVoxelColor(int verticalLayer, int horizontalOffSet, Vector3 direcrion)
     {
         MeshCollider meshCollider = GetComponentInChildren<MeshCollider>();
         
-        float vox = _voxelSize;
-        float half = _voxelSize / 2;
+        float vox = VoxelSize;
+        float half = VoxelSize / 2;
 
         Vector3 rayStart ;
         
@@ -58,25 +56,21 @@ public class VoxelTile : MonoBehaviour
         else if (direcrion == Vector3.left)
         {
             rayStart = meshCollider.bounds.max + 
-                       new Vector3(half, 0, -half - (_tileSideVoxels - horizontalOffSet -1) * vox);
+                       new Vector3(half, 0, -half - (TileSideVoxels - horizontalOffSet -1) * vox);
         }
         else if (direcrion == Vector3.back)
         {
             rayStart = meshCollider.bounds.max +
-                       new Vector3(-half - (_tileSideVoxels - horizontalOffSet -1) * vox, 0, half);
+                       new Vector3(-half - (TileSideVoxels - horizontalOffSet -1) * vox, 0, half);
         }
         else
         {
             throw new ArgumentException("Tou suck",  nameof(direcrion));
            
         }
-
         rayStart.y = meshCollider.bounds.min.y + half + verticalLayer * vox;
-       
 
-        Debug.DrawRay(rayStart, direcrion * 0.1f, Color.red, 3);
-
-        if (Physics.Raycast(new Ray(rayStart, direcrion), out RaycastHit hit, _voxelSize))
+        if (Physics.Raycast(new Ray(rayStart, direcrion), out RaycastHit hit, VoxelSize))
         {
             Mesh mesh = meshCollider.sharedMesh;
             
@@ -85,7 +79,6 @@ public class VoxelTile : MonoBehaviour
 
             return colorIndex;
         }
-
         return 0;
     }
 }
---
+++ Assets/Scripts/VoxelTilePlacer.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class VoxelTilePlacer : MonoBehaviour
+{
+    public VoxelTile[] TilePrefabs;
+    public Vector2Int MapSize = new Vector2Int(10, 10);
+
+    private VoxelTile[,] spawnedTiles;
+    private Coroutine tileInstantiated;
+    
+    
+    private void Start()
+    {
+        spawnedTiles = new VoxelTile[MapSize.x, MapSize.y];
+
+        foreach (VoxelTile tilePrefab in TilePrefabs)
+        {
+            tilePrefab.CalculateSideColors();
+        }
+        
+        tileInstantiated = StartCoroutine(Generate());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StopCoroutine(tileInstantiated);
+            
+            foreach (VoxelTile spawnedTile in spawnedTiles)
+            {
+                if (spawnedTile != null) Destroy(spawnedTile.gameObject);
+            }
+
+            StartCoroutine(Generate());
+        }
+    }
+
+    public IEnumerator Generate()
+    {
+        for (int x = 1; x < MapSize.x-1; x++)
+        {
+            for (int y = 1; y < MapSize.y-1; y++)
+            {
+                yield return new WaitForSeconds(0.1f);
+                PlaceTile(x, y);
+            }
+        }
+    }
+
+    public void PlaceTile(int x, int y)
+    {
+        List<VoxelTile> avelibleTiles = new List<VoxelTile>();
+
+        foreach (VoxelTile tilePrefab in TilePrefabs)
+        {
+            if (CanAppendTile(spawnedTiles[x - 1, y], tilePrefab, Vector3.left) &&
+                CanAppendTile(spawnedTiles[x + 1, y], tilePrefab, Vector3.right) &&
+                CanAppendTile(spawnedTiles[x, y - 1], tilePrefab, Vector3.back) &&
+                CanAppendTile(spawnedTiles[x, y + 1], tilePrefab, Vector3.forward))
+            {
+                avelibleTiles.Add(tilePrefab);
+            }
+        }
+
+        if (avelibleTiles.Count == 0) return;
+        
+        VoxelTile selectTile = avelibleTiles[Random.Range(0, avelibleTiles.Count)];
+        Vector3 position = new Vector3(x, 0, y) * (selectTile.VoxelSize * selectTile.TileSideVoxels);
+        spawnedTiles[x, y] = Instantiate(selectTile, position, Quaternion.identity);
+    }
+
+    private bool CanAppendTile(VoxelTile existTile, VoxelTile tileToAppend, Vector3 direction)
+    {
+        if (existTile == null) return true;
+
+        if (direction == Vector3.right)
+        {
+            return Enumerable.SequenceEqual(existTile.ColorRight, tileToAppend.ColorLeft);
+        }
+        else if (direction == Vector3.left)
+        {
+            return Enumerable.SequenceEqual(existTile.ColorLeft, tileToAppend.ColorRight);
+        }
+        else if (direction == Vector3.forward)
+        {
+            return Enumerable.SequenceEqual(existTile.ColorForward, tileToAppend.ColorBack);
+        }
+        else if (direction == Vector3.back)
+        {
+            return Enumerable.SequenceEqual(existTile.ColorBack, tileToAppend.ColorForward);
+        }
+        else
+        {
+            throw new ArgumentException("You Vector Suck", nameof(direction));
+        }
+    }
+    
+    
+}
